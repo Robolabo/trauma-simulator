3a484430d4bff80b9c2fb37f2501791f70655e60
Laura <lauraa.fdez24@gmail.com>
2022-07-16 20:35:56 +0200

    final 2

--- frontend/src/Simulations/Components/Actions.js
+++ frontend/src/Simulations/Components/Actions.js
@@ -955,17 +955,7 @@ class Actions extends Component {
         this.fillEvaluation(70)
     }
 
-   /* voluven(){
-        this.setState({
-            voluvenClicked:true
-        });
-        
-        this.props.change("diastolicPressure", 0.7693,780,1,20)
-        this.props.change("sistolicPressure", 1.1539,780,1,20)
-        this.getMsg("info",71)
-        this.fillInformation("Voluven 250ml IV")
-        this.fillEvaluation(71)
-    }*/
+
 
     sg(){
         this.setState({
@@ -2151,7 +2141,6 @@ voluven() {
                             <Button className={this.state.fentaniloClicked?"clicked":null} onClick={() => this.fentanilo()}>{t('simulation.fentanilo')}</Button>
                             <Button className={this.state.SSFClicked?"clicked":null} onClick={() => this.SSF()}>{t('simulation.SSF')}</Button>
                             <Button className={this.state.SSF500Clicked?"clicked":null} onClick={() => this.SSF500()}>{t('simulation.SSF500')}</Button>
-                            
                             {/*<Button onClick={() => this.cristaloides()}>Cristaloides</Button>*/}
                             
                          </div>
@@ -2387,14 +2376,13 @@ voluven() {
                             <Button className={this.state.fentaniloClicked?"clicked":null} onClick={() => this.fentanilo()}>{t('simulation.fentanilo')}</Button>
                             <Button className={this.state.SSFClicked?"clicked":null} onClick={() => this.SSF()}>{t('simulation.SSF')}</Button>
                             <Button className={this.state.SSF500Clicked?"clicked":null} onClick={() => this.SSF500()}>{t('simulation.SSF500')}</Button>
-                            <Button className={this.state.massivetransfusionClicked?"clicked":null} onClick={() => this.massivetransfusion()}>{t('simulation.massivetransfusion')}</Button>
-                            
+                            <Button className={this.state.voluvenClicked?"clicked":null} onClick={() => this.voluven()}>{t('simulation.voluven')}</Button>
                             
                          </div>
 
                          <div className="actions-buttons">
                             
-                            
+                            <Button className={this.state.massivetransfusionClicked?"clicked":null} onClick={() => this.massivetransfusion()}>{t('simulation.massivetransfusion')}</Button>
                             <Button className="btn-hidden"></Button>
                             <Button className="btn-hidden"></Button>
                             <Button className="btn-hidden"></Button>
